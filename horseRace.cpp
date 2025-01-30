51f19bbe551807896f1d655a10bb4fb91fe2595d
Jo Cropper <joseph.cropper@bsu.edu>
2025-01-30 16:59:36 -0500

    The horses (they race)
    the horses DO be racing

---
+++ horseRace.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+class Horse{
+    public:
+        int name;
+        int pos = 0;
+
+        bool move(int flip){
+            if (flip == 1){
+                pos++;
+                if (pos == 15){
+                    printLane();
+                    cout << "\n";
+                    cout << name;
+                    cout << " has won!";
+                    return true;
+                }
+            }
+            printLane();
+            return false;
+        }
+        
+        void printLane(){
+            cout << "\n";
+            for (int u = 0; u < pos; u++){
+                cout << ".";
+            }
+            cout << name;
+            for (int u = pos; u < 14; u++){
+                cout << ".";
+            }
+        }
+};
+
+int main() {
+    srand(time(0));
+    Horse horse[5];
+    bool raceContinue = true, waitForInput = true;
+    
+    for (int u = 0; u < 5; u++){
+        horse[u].name = u;
+    }
+    
+    while (raceContinue){
+        cout << "\nENTER to continue: ";
+        cin.ignore();
+        for (int u = 0; u < 5; u++){
+            if (horse[u].move((rand()%2)) == true){
+                raceContinue = false;   
+            }
+        }
+    }
+    return 0;
+}
